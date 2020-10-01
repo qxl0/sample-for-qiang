22fe8a7d1d6124ef5447d13feee75aa5083dc208
Qiang <qxl0@yahoo.com>
2020-10-01 11:21:06 -0400

    Added getter and setter of field name

--- src/com/qiang/User.java
+++ src/com/qiang/User.java
@@ -2,4 +2,14 @@ package com.qiang;
 
 public class User {
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
