2ad0d408d64fb2abe8e92d3efd5453a4d198b110
Qiang <qxl0@yahoo.com>
2020-10-01 11:32:42 -0400

    Updated the toString method

--- src/com/qiang/User.java
+++ src/com/qiang/User.java
@@ -12,9 +12,11 @@ public class User {
 		this.name = name;
 	}
 
+	
+
 	@Override
 	public String toString() {
-		return "User [name=" + name + "]";
+		return "User [name=" + name + ", age=" + age + "]";
 	}
 
 	public int getAge() {
